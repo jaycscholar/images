d24f988448274a39e3608cc737ee44143638b332
Jay R Barrios <reyezsj@gmail.com>
2025-10-12 01:15:27 -0700

    added index file

---
+++ navigation.js
@@ -0,0 +1,13 @@
+
+const navigationHeader = document.createElement("header");
+navigationHeader.innerHTML = `
+<nav>  
+     <a href="index.html">Home</a>
+     <a href="photos.html">Photos</a>
+     <a href="heroes.html">Heroes sample</a>
+
+</nav>
+`;
+
+document.body.appendChild(navigationHeader);
+
